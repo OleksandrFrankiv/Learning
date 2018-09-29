cda705f1dd9531a17b902f124ea39cc904920544
OleksandrFrankiv <oleksandr.frankiv@gmail.com>
2018-09-29 12:20:52 +0300

    Fibonacci

--- ex4/script.js
+++ ex4/script.js
@@ -2,8 +2,8 @@ function fibonacci( num ){
     var fibmas = [ 0, 1 ];
     for( var i = 2; i <= num; i++ ){
             fibmas[i] = fibmas[ i - 1 ] + fibmas[ i - 2 ];
-            console.log( fibmas )
         }
+        return fibmas[num]
     }
 
 console.log(fibonacci( 10 ))
