e2677387377f65bcb00f6096f9a5210a2d7a73b9
OleksandrFrankiv <oleksandr.frankiv@gmail.com>
2018-10-29 22:32:12 +0200

    Removed useless folder

--- deb1/script.js
+++
@@ -1,9 +0,0 @@
-debugger
-function greet(person) {
-  if (person.name ==  'amy' ) {
-    return console.log('hey amy')
-  } else {
-    return console.log('hey arnold')
-  }
-}
-greet({ name: 'amy' })
